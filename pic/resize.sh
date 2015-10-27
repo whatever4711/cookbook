c38d7528ae7ab0a89174aaebafbb40ae8890d70e
Marcel Großmann <whatever4711@gmail.com>
2015-10-27 15:15:38 +0100

    Updated Resize for 1024x768 or 768x1024

--- pic/resize.sh
+++ pic/resize.sh
@@ -2,9 +2,10 @@
 
 for i in *.JPG;
 do
-	if [ $(identify $i | cut -f 3 -d " ") != "1024x768" ];
-	then
-		echo "Resizing $i"
-		mogrify -resize 1024x768 -quality 75 $i
-	fi	       
+	convert $i -resize 786432@ -quality 75 $i
+#	if [ $(identify $i | cut -f 3 -d " ") != "1024x768" ];
+#	then
+#		echo "Resizing $i"
+#		mogrify -resize 1024x768 -quality 75 $i
+#	fi	       
 done
